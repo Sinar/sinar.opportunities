15af4b673370034bf57304cc7510c5039bbc8c81
Khairil Yusof <khairil.yusof@gmail.com>
2025-04-29 07:39:37 +0800

    Add vocabulary: Opportunities

---
+++ src/sinar/opportunities/tests/test_vocab_opportunities.py
@@ -0,0 +1,31 @@
+from sinar.opportunities import _
+from sinar.opportunities.testing import SINAR_OPPORTUNITIES_INTEGRATION_TESTING  # noqa
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from zope.component import getUtility
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.interfaces import IVocabularyTokenized
+
+import unittest
+
+
+class OpportunitiesIntegrationTest(unittest.TestCase):
+
+    layer = SINAR_OPPORTUNITIES_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def test_vocab_opportunities(self):
+        vocab_name = 'sinar.opportunities.Opportunities'
+        factory = getUtility(IVocabularyFactory, vocab_name)
+        self.assertTrue(IVocabularyFactory.providedBy(factory))
+
+        vocabulary = factory(self.portal)
+        self.assertTrue(IVocabularyTokenized.providedBy(vocabulary))
+        self.assertEqual(
+            vocabulary.getTerm('sony-a7r-iii').title,
+            _(u'Sony Aplha 7R III'),
+        )
