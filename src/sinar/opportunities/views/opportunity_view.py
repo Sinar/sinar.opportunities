131dee34bc39e0f5b32ed2fe4e7ff884662f72c1
Khairil Yusof <khairil.yusof@gmail.com>
2025-05-08 18:53:56 +0800

    Add initial view for Opportunity content type
    Fixes https://github.com/Sinar/sinar.opportunities/issues/2

--- src/sinar/opportunities/views/opportunity_view.py
+++ src/sinar/opportunities/views/opportunity_view.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 # from sinar.opportunities import _
-from Products.Five.browser import BrowserView
+from zope.publisher.browser import BrowserView
+from zope.component import getUtility
+from zope.schema.interfaces import IVocabularyFactory
 from zope.interface import implementer
 from zope.interface import Interface
 
@@ -16,6 +18,19 @@ class OpportunityView(BrowserView):
     # If you want to define a template here, please remove the template from
     # the configure.zcml registration of this view.
     # template = ViewPageTemplateFile('opportunity_view.pt')
+
+    def opportunity_type_titles(self):
+
+        factory = getUtility(IVocabularyFactory,
+                             'sinar.opportunities.opportunity_types')
+
+        vocabulary = factory(self)
+        if self.context.opportunity_types:
+            titles = []
+            for opportunity_type in self.context.opportunity_types:
+                term = vocabulary.getTerm(opportunity_type)
+                titles.append(term.title)
+            return titles
 
     def __call__(self):
         # Implement your own actions:
